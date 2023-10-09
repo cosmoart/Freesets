c4c6fad460164b1b6220c09e6d0bec0583eb4034
CosmoArt <cosmohydra17@gmail.com>
2023-10-08 21:21:57 -0500

    🐞 fix: production rollup

--- source-web/svelte.config.js
+++ source-web/svelte.config.js
@@ -8,9 +8,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		// alias: {
-		// 	'@/': 'src/'
-		// }
+		alias: {
+			'@/': 'src/'
+		}
 	},
 	preprocess: vitePreprocess()
 };
