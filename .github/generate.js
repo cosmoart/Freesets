47041412eedec1e039d7e54f938d17e1c70e00f3
CosmoArt <cosmohydra17@gmail.com>
2024-06-14 15:40:54 -0500

    🐞 fix: broken links

--- .github/generate.js
+++ .github/generate.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+// Output md
 const dbFolderPath = path.join(__dirname, '../source-web/src/assets/db')
 const outputFilePath = path.join(__dirname, 'output.md')
 
@@ -20,5 +21,18 @@ fs.readdir(dbFolderPath, (err, files) => {
 	fs.writeFile(outputFilePath, links.join('\n'), err => {
 		if (err) return console.error('Error writing Markdown file: ', err)
 		console.log('Markdown file generated successfully.')
+	})
+})
+
+// Ignore links
+const ignoreOutputPath = path.join(__dirname, '../.lycheeignore')
+const ignoreFilePath = path.join(__dirname, './.lycheeignore')
+
+fs.readFile(ignoreFilePath, "utf8", (err, file) => {
+	if (err) return console.error('Error reading folder: ', err)
+
+	fs.writeFile(ignoreOutputPath, file, err => {
+		if (err) return console.error('Error writing Markdown file: ', err)
+		console.log('Ignore file generated successfully.')
 	})
 })
